a6ab81f0f7a81ea7da38426053037b539f790a46
Reinardus Redo Haryono <rredoh.rrh@gmail.com>
2026-02-10 22:27:22 +0100

    Update Valentine's messages and page title

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ const CONFIG = {
 
     // The title that appears in the browser tab
     // You can use emojis! 💝 💖 💗 💓 💞 💕
-    pageTitle: "Will You Be My Valentine? 💝",
+    pageTitle: "A Small Valentine Question 💝",
 
     // Floating emojis that appear in the background
     // Find more emojis at: https://emojipedia.org
@@ -32,7 +32,7 @@ const CONFIG = {
             nextBtn: "Next ❤️"                                         // Text for the next button
         },
         third: {
-            text: "Will you be my Valentine on February 14th, 2025? 🌹", // The big question!
+            text: "Would you be my Valentine this year? 🌹",            // The big question!
             yesBtn: "Yes!",                                             // Text for "Yes" button
             noBtn: "No"                                                 // Text for "No" button
         }
@@ -41,16 +41,16 @@ const CONFIG = {
     // Love meter messages
     // They show up depending on how far they slide the meter
     loveMessages: {
-        extreme: "WOOOOW You love me that much?? 🥰🚀💝",  // Shows when they go past 5000%
-        high: "To infinity and beyond! 🚀💝",              // Shows when they go past 1000%
-        normal: "And beyond! 🥰"                           // Shows when they go past 100%
+        extreme: "Okay wow, that escalated 😄💖",  // Shows when they go past 5000%
+        high: "That’s dangerously high 👀💝",              // Shows when they go past 1000%
+        normal: "I’ll take it 😊"                           // Shows when they go past 100%
     },
 
     // Messages that appear after they say "Yes!"
     celebration: {
-        title: "Yay! I'm the luckiest person in the world! 🎉💝💖💝💓",
-        message: "Now come get your gift, a big warm hug and a huge kiss!",
-        emojis: "🎁💖🤗💝💋❤️💕"  // These will bounce around
+        title: "This just made my day 💝",
+        message: "Let’s count this as a rain check for something better in person.",
+        emojis: "💖✨🤍"  // These will bounce around
     },
 
     // Color scheme for the website
