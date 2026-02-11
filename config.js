6cf3607222c77c3804ecbffd1b180566527f334d
Reinardus Redo Haryono <rredoh.rrh@gmail.com>
2026-02-11 04:44:07 +0100

    Update music streaming URL in config.js

--- config.js
+++ config.js
@@ -77,7 +77,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://res.cloudinary.com/dncywqfpb/video/upload/v1738399057/music_qrhjvy.mp3", // Music streaming URL
+        musicUrl: "https://res.cloudinary.com/dcgrd3xei/video/upload/v1770781406/Westlife_-_How_Does_It_Feel_Official_Audio_-_westlifeVEVO_ynuuaj.mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
